f011711b815a81a4e6435f2ee5000d2776bf3431
acet09 <ysjungman@gmail.com>
2023-05-14 07:38:34 +0900

    249_페이로드_연결

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -15,6 +15,10 @@ const Counter = () => {
     dispatch({ type: 'increment' });
   };
 
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 10 })
+  };
+
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
@@ -27,6 +31,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button >
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
